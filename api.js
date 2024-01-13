87113997742121f923d08060c13115d0e157cc85
Daniil Nikitas <daniil_nikitas@epam.com>
2024-01-13 22:05:41 +0600

    fixed api body-parser

--- api.js
+++ api.js
@@ -6,15 +6,15 @@ import getScrapedData from './scraper.js'
 const app = express()
 const port = 3000
 const hostname = '0.0.0.0'
-app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended:true}));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy', "default-src 'self' *.ozone.ru *.ozonusercontent.com  http://localhost:3000 *.ozon.ru *.kz.ozon.com *.ozon.by *.ozon.kz *.ozonru.me *.by-stg.ozonru.me *.kz-stg.ozoncom.me enterprise.api-maps.yandex.ru wss:");
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
   next();
 });
 app.post('/api/scraped-data', async (request, response) => {
