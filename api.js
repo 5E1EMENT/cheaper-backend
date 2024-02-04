025a3babca81ac7f7fea972c0a8816653a3b1e0f
Daniil Nikitas <daniil_nikitas@epam.com>
2024-02-04 17:33:39 +0600

    Added port env var

--- api.js
+++ api.js
@@ -4,9 +4,10 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import getScrapedData from './scraper.js'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000
 const hostname = '0.0.0.0'
 app.use(cors());
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy', "default-src 'self' *.ozone.ru *.ozonusercontent.com  http://localhost:3000 *.ozon.ru *.kz.ozon.com *.ozon.by *.ozon.kz *.ozonru.me *.by-stg.ozonru.me *.kz-stg.ozoncom.me enterprise.api-maps.yandex.ru wss:");
   next();
 });
+
 app.get('/api/scraped-data', async (request, response) => {
     const searchProduct = request.query.searchProduct
     const parsedData = await getScrapedData(searchProduct)
