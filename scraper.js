cc694a36a6b32da518421c51cf5698fa337a6ec4
Daniil Nikitas <daniil_nikitas@epam.com>
2024-02-18 01:19:08 +0600

    fixed puppeteer

--- scraper.js
+++ scraper.js
@@ -29,6 +29,8 @@ export default async function getScrapedData(searchProduct) {
     const pages = []
     for (let origin of ORIGINS) {
       const page = await browser.newPage();
+      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
       const url = getCurrentUrl(unifiedName, origin)
       console.log('url', url)
       await page.goto(url, { waitUntil: 'domcontentloaded' });
