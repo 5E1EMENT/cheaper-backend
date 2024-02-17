b49bf36a756fad0738ccb04a7f348378d0407691
Daniil Nikitas <daniil_nikitas@epam.com>
2024-02-18 00:15:57 +0600

    test without Content-Security-Policy

--- api.js
+++ api.js
@@ -15,7 +15,7 @@ app.use(
   }),
 );
 app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', "default-src 'self' *.ozone.ru *.ozonusercontent.com  http://localhost:3000 *.ozon.ru *.kz.ozon.com *.ozon.by *.ozon.kz *.ozonru.me *.by-stg.ozonru.me *.kz-stg.ozoncom.me enterprise.api-maps.yandex.ru wss:");
+  // res.setHeader('Content-Security-Policy', "default-src 'self' *.ozone.ru *.ozonusercontent.com  http://localhost:3000 *.ozon.ru *.kz.ozon.com *.ozon.by *.ozon.kz *.ozonru.me *.by-stg.ozonru.me *.kz-stg.ozoncom.me enterprise.api-maps.yandex.ru wss:");
   next();
 });
 
