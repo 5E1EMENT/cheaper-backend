f386f89e3d845c0d05a11be4a6e7832b10a86572
Daniil Nikitas <daniil_nikitas@epam.com>
2024-02-27 20:18:55 +0600

    Optimized for of, added promise all in scraper and get products data

--- functions/getProductsData.js
+++ functions/getProductsData.js
@@ -5,23 +5,24 @@ import {scrapeSatu} from '../scrapers/scrapeSatu.js';
 
 
 export const getProductsData = async (pages, searchProduct) => {
-    let result = []
-    const functions = [];
-    // Вызываем все функции скрапинга параллельно
-    for (let page of pages) {
-        console.log('page.origin', page.origin)
-        if (page.origin === 'ozon.kz') {
-            functions.push(scrapeOzon(page.page))
-        } else if (page.origin === 'wildberries.ru') {
-            functions.push(scrapeWildberries(page.page, searchProduct))
-        } else if (page.origin === 'satu.kz') {
-            functions.push(scrapeSatu(page.page))
-        }
+    const scraperFunctions = {
+      'ozon.kz': scrapeOzon,
+      'wildberries.ru': scrapeWildberries,
+      'satu.kz': scrapeSatu,
+      // Добавьте другие функции скрапинга, если необходимо
     };
-
-        const [satuResult, wildberriesResult, ozonResult] = await Promise.all(functions);
-        console.log('satuResult', satuResult.length, 'wildberriesResult', wildberriesResult.length, 'ozonResult', ozonResult.length)
-        result.push(...ozonResult, ...wildberriesResult, ...satuResult)
-
-        return result;
-}
+  
+    const scraperPromises = pages.map(async (page) => {
+      console.log('page.origin', page.origin);
+      const scraperFunction = scraperFunctions[page.origin];
+      if (!scraperFunction) {
+        throw new Error(`Unsupported origin: ${page.origin}`);
+      }
+      return scraperFunction(page.page, searchProduct);
+    });
+  
+    const results = await Promise.all(scraperPromises);
+    const flattenedResults = [].concat(...results);
+  
+    return flattenedResults;
+  };
