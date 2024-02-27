4346c14d574feecf374c78564773b0fb01402887
Daniil Nikitas <daniil_nikitas@epam.com>
2024-02-27 21:10:17 +0600

    test scraper 222

--- scraper.js
+++ scraper.js
@@ -26,28 +26,30 @@ export default async function getScrapedData(searchProduct) {
     console.log('start')
     const unifiedName = await fetchDataWithRetry((searchProduct) => chatController2.getUnifiedProductName(searchProduct), searchProduct);
     console.log('unifiedName', unifiedName)
-    const pages = []
-    const customHeaders = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-      'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-      'Origin': 'https://global.wildberries.ru',
-      'Referer': 'https://global.wildberries.ru',
-      // Другие заголовки, если необходимо
-    };
-    for (let origin of ORIGINS) {
-      const page = await browser.newPage();
+    const createPage = async (origin) => {
+      const customHeaders = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Origin': 'https://global.wildberries.ru',
+        'Referer': 'https://global.wildberries.ru',
+        // Другие заголовки, если необходимо
+      };
     
+      const page = await browser.newPage();
       await page.setExtraHTTPHeaders(customHeaders);
-
-      const url = getCurrentUrl(unifiedName, origin)
-      console.log('url', url)
+    
+      const url = getCurrentUrl(unifiedName, origin);
+      console.log('url', url);
+    
       await page.goto(url, { waitUntil: 'domcontentloaded' });
-      pages.push({
-        origin,
-        page
-      })
+    
       page.on('console', msg => console.log('CONSOLE LOG:', msg.text()));
-    }
+    
+      return { origin, page };
+    };
+    
+    const promises = ORIGINS.map(origin => createPage(origin));
+    const pages = await Promise.all(promises);
 
     const productsData = await getProductsData(pages, unifiedName)
     const productNames = productsData.map(item => item.name)
